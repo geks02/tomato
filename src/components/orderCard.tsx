3d0aa1e40c0d5a9ed1684702b0375689303d3740
Dark Code <dark@Darks-MacBook-Air.local>
2026-05-14 14:44:37 +0530

    Initial Commit

--- src/components/orderCard.tsx
+++ src/components/orderCard.tsx
@@ -4,7 +4,6 @@ import { ORDER_ACTIONS } from "../utils/orderflow";
 import axios from "axios";
 import { restaurantService } from "../main";
 import toast from "react-hot-toast";
-import Orders from "../pages/Orders";
 
 interface props {
   order: IOrder;
